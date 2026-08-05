df0291f28928e48b05c8bbedcb328af5758b5cfa
OneBruno <ms2689992@gmail.com>
2026-08-05 20:31:13 -0300

    Chore: Configurando a injeção de dependencias

--- SaborCia.API/Program.cs
+++ SaborCia.API/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using SaborCia.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
@@ -6,6 +10,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+        ));
 
 var app = builder.Build();
 
