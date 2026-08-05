09ab204ae6ad0985bb49cbeccf68276c4b3478d4
OneBruno <ms2689992@gmail.com>
2026-08-05 19:29:34 -0300

    Feat: Criando models e configurando contexto do banco de dados

---
+++ SaborCia.API/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SaborCia.API.Models;
+
+namespace SaborCia.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Unidades> Unidades { get; set; }
+        public DbSet<Produtos> Produtos { get; set; }
+        public DbSet<Usuarios> Usuarios { get; set; }
+        public DbSet<Pedidos> Pedidos { get; set; }
+        public DbSet<ItemPedidos> ItemPedidos { get; set; }
+        public DbSet<Estoque> Estoque { get; set; }
+    }
+
+    
+}
