a80ce483c47a6a09984fb7c22c6a38434f4a2e6c
OneBruno <ms2689992@gmail.com>
2026-08-07 15:22:39 -0300

    Chore: Configurando a DTO Criar Usuário

---
+++ SaborCia.API/DTOs/CriarUsuarioDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SaborCia.API.DTOs
+{
+    public class CriarUsuarioDto
+    {
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Seenha { get; set; } = string.Empty;
+        public string RoleUsuario { get; set; } = string.Empty;
+        public int IdUnidade { get; set; }
+    }
+}
