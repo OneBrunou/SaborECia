77f6f835354167202412a7e9f6cf3ae805008480
OneBruno <ms2689992@gmail.com>
2026-08-07 12:26:45 -0300

    Feat: Criando a controller Produtos configurando a listagem de produtos

---
+++ SaborCia.API/Controllers/ProdutosController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SaborCia.API.Data;
+
+namespace SaborCia.API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task <IActionResult> GetPrdutos()
+        {
+            var produtos = await _context.Produtos.ToListAsync();
+            return Ok(produtos);
+        }
+    }
+}
