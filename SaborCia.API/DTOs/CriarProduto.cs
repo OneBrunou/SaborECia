2e0e37f5fde2f75611ba52d93104ace2d409f687
OneBruno <ms2689992@gmail.com>
2026-08-07 12:02:09 -0300

    Feat: Adcionando a DTO Produto

---
+++ SaborCia.API/DTOs/CriarProduto.cs
@@ -0,0 +1,10 @@
+﻿namespace SaborCia.API.DTOs
+{
+    public class CriarProduto
+    {
+        public string Nome { get; set; } = string.Empty;
+        public string? Descricao { get; set;  }
+        public decimal Preco { get; set; }
+        public string? Categoria { get; set; }
+    }
+}
