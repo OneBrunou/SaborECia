a78707a0015b988c55f6698a79d6b8d60723c147
OneBruno <ms2689992@gmail.com>
2026-08-07 16:25:37 -0300

    Refactor: Adicionando a etapa de verificação de email no DTO

--- SaborCia.API/DTOs/AtualizarUsuarioDto.cs
+++ SaborCia.API/DTOs/AtualizarUsuarioDto.cs
@@ -1,8 +1,12 @@
-﻿namespace SaborCia.API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SaborCia.API.DTOs
 {
     public class AtualizarUsuarioDto
     {
         public string Nome { get; set; } = string.Empty;
+        [Required(ErrorMessage = "O email é obrigatório")]
+        [EmailAddress(ErrorMessage = "Email em formato invalido")]
         public string Email { get; set; } = string.Empty;
         public string RoleUsuario { get; set; } = string.Empty;
         public int IdUnidade { get; set; }
