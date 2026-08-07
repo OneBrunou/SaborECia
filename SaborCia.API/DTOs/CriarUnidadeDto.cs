17f3d1e9f65e64a266dddeb773e281fd3c43ad47
OneBruno <ms2689992@gmail.com>
2026-08-07 10:11:25 -0300

    Feat: Criando e configurando a DTO Unidade

---
+++ SaborCia.API/DTOs/CriarUnidadeDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SaborCia.API.DTOs
+{
+    public class CriarUnidadeDto
+    {
+        public string Nome { get; set; } = string.Empty;
+        public string Endereco { get; set; } = string.Empty;
+    }
+}
