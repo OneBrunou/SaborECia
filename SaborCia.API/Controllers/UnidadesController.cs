079bee93dc8c24c13a10e3b54b7d927e12759914
OneBruno <ms2689992@gmail.com>
2026-08-07 11:29:26 -0300

    Feat: Construindo o CRUD das Unidades

--- SaborCia.API/Controllers/UnidadesController.cs
+++ SaborCia.API/Controllers/UnidadesController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using SaborCia.API.Data;
+using SaborCia.API.DTOs;
+using SaborCia.API.Models;
 
 namespace SaborCia.API.Controllers
 {
@@ -20,6 +23,56 @@ namespace SaborCia.API.Controllers
             var unidades = await _context.Unidades.ToListAsync();
             return Ok(unidades);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CriarUnidade(CriarUnidadeDto dto)
+        {
+            var unidade = new Unidades
+            {
+                Nome = dto.Nome,
+                Endereco = dto.Endereco
+            };
+
+            _context.Unidades.Add(unidade);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUnidades), new { id = unidade.Id }, unidade);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> AtualizarUnidade(int id, AtualizarUnidade dto)
+        {
+            var unidade = await _context.Unidades.FindAsync(id);
+
+            if (unidade == null)
+            {
+                return NotFound();
+            }
+
+            unidade.Nome = dto.Nome;
+            unidade.Endereco = dto.Endereco;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletarUnidade(int id)
+        {
+            var unidade = await _context.Unidades.FindAsync(id);
+
+            if (unidade == null)
+            {
+                return NotFound();
+
+            }
+
+            _context.Unidades.Remove(unidade);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
     }
    
 }
---
+++ SaborCia.API/DTOs/AtualizarUnidade.cs
@@ -0,0 +1,8 @@
+﻿namespace SaborCia.API.DTOs
+{
+    public class AtualizarUnidade
+    {
+        public string Nome { get; set; } = string.Empty;
+        public string Endereco { get; set; } = string.Empty;
+    }
+}
