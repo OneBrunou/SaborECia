6c619f6ec909367ac36bba6db697db56ca45753c
OneBruno <ms2689992@gmail.com>
2026-08-07 15:52:28 -0300

    Feat: Adicionando a função de Criar Usuario

---
+++ SaborCia.API/Controllers/UsuarioController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Org.BouncyCastle.Crypto.Generators;
+using BCrypt.Net;
+using SaborCia.API.Data;
+using SaborCia.API.DTOs;
+using SaborCia.API.Models;
+
+namespace SaborCia.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public UsuarioController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsuarios()
+        {
+            var usuarios = await _context.Usuarios.ToListAsync();
+            return Ok(usuarios);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CriarUsuario(CriarUsuarioDto dto)
+        {
+            var usuario = new Usuarios
+            {
+                Nome = dto.Nome,
+                Email = dto.Email,
+                SenhaHash = BCrypt.Net.BCrypt.HashPassword(dto.Senha),
+                RoleUsuario = Enum.Parse<RoleUsuario>(dto.RoleUsuario),
+                IdUnidade = dto.IdUnidade,
+                CriadoEm = DateTime.Now
+            };
+
+            _context.Usuarios.Add(usuario);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUsuarios), new { id = usuario.Id }, usuario);
+        }
+    }
+}
--- SaborCia.API/DTOs/CriarUsuarioDto.cs
+++ SaborCia.API/DTOs/CriarUsuarioDto.cs
@@ -4,7 +4,7 @@
     {
         public string Nome { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string Seenha { get; set; } = string.Empty;
+        public string Senha { get; set; } = string.Empty;
         public string RoleUsuario { get; set; } = string.Empty;
         public int IdUnidade { get; set; }
     }
